f3dbff477ed39fc52d24f9a98c247f5e5157b56b
PrernaRathore10 <prerna25io043@satiengg.in>
2024-12-25 22:43:50 +0530

    performing migrations

--- Project1/Project1/urls.py
+++ Project1/Project1/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -27,5 +29,5 @@ urlpatterns = [
     path('app001/', include('app001.urls')),
 
 # ensure that it stays at last
-    path("__reload__/", include("django_browser_reload.urls"))
-]
+    path("__reload__/", include("django_browser_reload.urls")),
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
